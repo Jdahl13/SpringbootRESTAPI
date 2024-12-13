fdeed449098b28986caeb743af566ab24da807ed
jdahl13 <jdahl07@gmail.com>
2024-12-12 21:22:10 -0800

    Added extracted springboot folder

---
+++ Springboot-API-Practice/src/main/java/com/example/Springboot/API/Practice/SpringbootApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot.API.Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiPracticeApplication.class, args);
+	}
+
+}
---
+++ Springboot-API-Practice/src/test/java/com/example/Springboot/API/Practice/SpringbootApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot.API.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
