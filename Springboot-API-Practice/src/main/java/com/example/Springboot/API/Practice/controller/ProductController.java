ebfb2a310a1c6c01ee84425b391c874306d53ed1
jdahl13 <jdahl07@gmail.com>
2024-12-13 13:39:21 -0800

    Started controller class, added all imports

---
+++ Springboot-API-Practice/src/main/java/com/example/Springboot/API/Practice/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.Springboot.API.Practice.controller;
+
+import com.example.Springboot.API.Practice.entity.Product;
+import com.example.Springboot.API.Practice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+
+public class ProductController {
+}
